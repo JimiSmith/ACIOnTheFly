f0a3e69dd8de579340a9862640a52c4d58500c21
James Smith <mail@jimismith.me>
2017-10-19 21:08:53 +0200

    Add project files.

---
+++ ACIOnTheFly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACIOnTheFly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
